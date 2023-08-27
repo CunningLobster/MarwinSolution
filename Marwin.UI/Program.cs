3704511e3e67a6837767433a4bc89114e8e1eab7
Yan Cherepansky <cherepansky.yan@yandex.ru>
2023-08-27 13:26:53 +0600

    Add services

--- Marwin.UI/Program.cs
+++ Marwin.UI/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Marwin.Core.Domain.RepositoryContracts;
+using Marwin.Core.ServiceContracts.CompanyServiceContracts;
+using Marwin.Core.ServiceContracts.EmployeeServiceContracts;
+using Marwin.Core.Services;
+using Marwin.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +14,35 @@ namespace Marwin.UI
 {
     internal static class Program
     {
+        public static IServiceProvider ServiceProvider { get; set; }
+
+        static void ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            //Добавление сервисов в IoC контейнер
+            services.AddSingleton<ApplicationDbContext, ApplicationDbContext>();
+            services.AddScoped<ICompanyRepository, CompaniesRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeesRepository>();
+
+            services.AddScoped<IEmployeeGetterService, EmployeeService>();
+            services.AddScoped<IEmployeeAdderService, EmployeeService>();
+            services.AddScoped<IEmployeeUpdaterService, EmployeeService>();
+            services.AddScoped<IEmployeeDeleterSevice, EmployeeService>();
+
+            services.AddScoped<ICompanyGetterService, CompanyService>();
+            services.AddScoped<ICompanyAdderService, CompanyService>();
+            services.AddScoped<ICompanyUpdaterService, CompanyService>();
+            services.AddScoped<ICompanyDeleterService, CompanyService>();
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        public static T GetService<T>() where T : class
+        {
+            return (T)ServiceProvider.GetService(typeof(T));
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,6 +51,7 @@ namespace Marwin.UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            ConfigureServices();
             Application.Run(new Form1());
         }
     }
